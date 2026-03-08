cea8695d2c2abddfc9d3531a053c8261dbb9f2ca
Guibson Valerio <guibsonv@icloud.com>
2026-03-08 13:54:04 -0300

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 ﻿window.APP_CONFIG = {
-  SUPABASE_URL: "https://YOUR-PROJECT-REF.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
+  SUPABASE_URL: "https://mzeekhlaehtjdewkldnl.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16ZWVraGxhZWh0amRld2tsZG5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5ODA5NDEsImV4cCI6MjA4ODU1Njk0MX0.SWAc3n0oqP9Ub9hxTbNae5ce2gZsAf8JfZP94Mjh_pk",
 };
+
